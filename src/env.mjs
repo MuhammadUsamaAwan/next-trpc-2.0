b65dba31c4f2d77054fa1646ea68a0d4c4861fd8
MuhammadUsamaAwan <underusama@gmail.com>
2023-09-08 16:39:29 +0500

    add redis

--- src/env.mjs
+++ src/env.mjs
@@ -1,11 +1,13 @@
-import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
+import { createEnv } from '@t3-oss/env-nextjs';
 
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(['development', 'test', 'production']),
     JWT_SECRET: z.string().min(1),
     JWT_EXPIRES_IN: z.string().min(1),
+    UPSTASH_REDIS_REST_URL: z.string().min(1),
+    UPSTASH_REDIS_REST_TOKEN: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().url(),
@@ -15,6 +17,8 @@ export const env = createEnv({
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 });
